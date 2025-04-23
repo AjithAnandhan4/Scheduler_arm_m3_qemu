da74262999a56fba650680807497eb4542e60bf5
AjithAnandhan4 <ajithanandhan0406@gmail.com>
2025-04-23 16:26:59 +0000

    Add systick implementation

--- main.c
+++ main.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
-#define _IO volatile 
-int g_var = 1234;
+#include <stdio.h>
+
+#define SYSTICK_CTRL (*(volatile uint32_t*)0xE000E010)
+#define SYSTICK_LOAD (*(volatile uint32_t*)0xE000E014)
+#define SYSTICK_VAL  (*(volatile uint32_t*)0xE000E018)
+
+volatile uint32_t g_tick_cntr = -1;
+
+void SysTick_Handler(void)
+{
+  g_tick_cntr++;
+}
+
+void init_systick(void)
+{
+  SYSTICK_LOAD = 8000 -1;
+  SYSTICK_VAL = 0;
+  SYSTICK_CTRL = 7U;
+}
 
 int main(void)
 {
-  for(_IO int i=0; i<20;i++)
+  init_systick();
+
+  while(1)
   {
-    g_var++; 
+    __asm volatile ("wfi");
   }
   return 0;
 }
